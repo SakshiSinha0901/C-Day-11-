5d8f520373461ec302a203a70daf5bcb9aea3626
Sakshi Sinha <128817418+SakshiSinha0901@users.noreply.github.com>
2024-06-13 22:14:25 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //IMPLICIT TYPE CASTING
+
+    // int num1 = 10;
+    // float num2 = 5.5;
+    // float result = num1 + num2 ;
+    // cout<<result<<endl;
+
+    // char ch = 'A';
+    // char a = ch + 1;
+    // cout<<a<<endl;
+
+    //EXPLICT TYPE CASTING
+    // int num1 = 20;
+    // int num2 = 6.5;
+    // float result = num1 + (int)num2;
+    // cout<<result<<endl;  
+
+    // int a = 10;
+    // int b = 3.0;
+    // float c = a /(float)b;
+    // cout<<c<<endl;
+
+    int a = 10;
+    int b = 3.0;
+    int c = a /(float)b;
+    cout<<c<<endl;
+}
+
